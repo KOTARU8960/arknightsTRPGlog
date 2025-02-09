974f98c838dddc4b72b5347a9d477b6cf773e3ed
KOTARU8960 <125034416+KOTARU8960@users.noreply.github.com>
2025-02-09 17:58:56 +0900

    Update log.js

--- log.js
+++ log.js
@@ -10,6 +10,7 @@ window.addEventListener('load', () => {
       const reader = new FileReader();
       const ab = /^[0-9()+-/*]{0,}AB&lt;=[0-9()+-/*]{0,}[ $]/;
       const ad = /^[0-9()+-/*]{0,}AD&lt;=[0-9()+-/*]{0,}[ $]/;
+      const num = /[1-9]/
       reader.onload = () => {
         var names = [];
         var result = []
@@ -24,48 +25,72 @@ window.addEventListener('load', () => {
 
         for ( let i = 0;  i < index;  i++  ){
             let textlen = text[16 + i*8].trim()
+
             if(ad.test(textlen)){
                 let namelen = text[14 + i*8];
                 let name = namelen.substring(namelen.indexOf(">")+1,namelen.lastIndexOf("<"))
+                if (!names.includes(name)){
+                    names.push(name);
+                    result.push([[0],[0],0]);
+                }
+                result[names.indexOf(name)][2]++;
                 if(textlen.endsWith("クリティカル！")){
-                    if (!names.includes(name)){
-                        names.push(name);
-                        result.push([[],[]]);
-                    }
                     result[names.indexOf(name)][0].push(textlen.replace(/&lt;/g,"<").replace(/&gt;/g,">"))
+                    result[names.indexOf(name)][0][0]++;
                 }
                 if(textlen.endsWith("エラー")){
-                    if (!names.includes(name)){
-                        names.push(name);
-                        result.push([[],[]]);
-                    }
                     result[names.indexOf(name)][1].push(textlen.replace(/&lt;/g,"<").replace(/&gt;/g,">"))
+                    result[names.indexOf(name)][1][0]++;
+                }
+            }
+
+            if(ab.test(textlen)){
+                let namelen = text[14 + i*8];
+                let name = namelen.substring(namelen.indexOf(">")+1,namelen.lastIndexOf("<"))
+                let cri = textlen[textlen.lastIndexOf("C")-1]
+                let err = textlen[textlen.lastIndexOf("E")-1]
+                if (!names.includes(name)){
+                    names.push(name);
+                    result.push([[0],[0],0]);
+                }
+                result[names.indexOf(name)][2]+=Number(textlen[textlen.lastIndexOf("AB")-1]);
+                if(num.test(cri)){
+                    result[names.indexOf(name)][0].push(textlen.replace(/&lt;/g,"<").replace(/&gt;/g,">"))
+                    result[names.indexOf(name)][0][0]+=Number(cri);
+                }
+                if(num.test(err)){
+                    result[names.indexOf(name)][1].push(textlen.replace(/&lt;/g,"<").replace(/&gt;/g,">"))
+                    result[names.indexOf(name)][1][0]+=Number(err);
                 }
             }
         }
 
         let ret = ""
         for (  let i = 0;  i < names.length;  i++  ){
-        ret = ret+"<h1># "+names[i]+"<br></h1>";
+        ret = ret+"<h1># "+names[i]+"(ロール回数："+result[i][2]+"回)</h1>";
 
-            if(result[i][0].length){
+            if(!(result[i][0].length+result[i][1].length-2)){
+                ret = ret+"<p>クリエラ無し！</p>";
+            }
+            if(result[i][0].length-1){
                 ret = ret+"<p>```</p>";
-                ret = ret+"<p>クリティカル : "+result[i][0].length+"回</p><br>";
-                for (  let k = 0;  k < result[i][0].length;  k++  ){
+                ret = ret+"<p>クリティカル : "+result[i][0][0]+"回("+Math.round((result[i][0][0]*100)/result[i][2])+"%)</p><br>";
+                for (  let k = 1;  k < result[i][0].length;  k++  ){
                     ret = ret+"<p>"+result[i][0][k]+"</p>";
                 }
                 ret = ret+"<p>```</p>";
             }
 
-            if(result[i][1].length){
+            if(result[i][1].length-1){
                 ret = ret+"<p>```</p>";
-                ret = ret+"<p>エラー : "+result[i][1].length+"回</p><br>";
-                for (  let k = 0;  k < result[i][1].length;  k++  ){
+                ret = ret+"<p>エラー : "+result[i][1][0]+"回("+Math.round((result[i][1][0]*100)/result[i][2])+"%)</p><br>";
+                for (  let k = 1;  k < result[i][1].length;  k++  ){
                     ret = ret+"<p>"+result[i][1][k]+"</p>";
                 }
                 ret = ret+"<p>```</p>";
             }
         }
+        
         pre.innerHTML = ret;
     };
   
